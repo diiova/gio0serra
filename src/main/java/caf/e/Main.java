283d2d166ee168c7d67a279d884a0a15e9bafca1
diiova <giovanni.serra@ltpdampierre.com>
2023-11-07 08:48:56 +0100

    first commit

---
+++ src/main/java/caf/e/Main.java
@@ -0,0 +1,26 @@
+package caf.e;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.printIn("Bonjour ! Je suis l'assistant de la machine à café. Dites-moi les pièces jaunes dont vous disposez :");
+
+        System.out.print("pièce de 1 cent ?");
+        int Cents1 = scanner.nextInt(5);
+
+        System.out.print("pièce de 5 cent ?");
+        int Cents5 = scanner.nextInt(5);
+
+        System.out.print("pièce de 10 cent ?");
+        int Cents10 = scanner.nextInt(2);
+
+        System.out.print("pièce de 20 cent ?");
+        int Cents20 = scanner.nextInt(0);
+
+        System.out.print("pièces de 50 cents ?");
+        int Cents50 = scanner.nextInt(1);
+    }
+}
